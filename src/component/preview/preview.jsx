e10310dc4bb9b9815b89aa7f9abb1076d45e9c86
KIMYEEUN22 <kye9702@naver.com>
2021-07-04 00:12:21 +0900

    add card and edit from component

--- src/component/preview/preview.jsx
+++ src/component/preview/preview.jsx
@@ -1,27 +1,16 @@
 import React from "react";
+import Card from "../card/card";
 import styles from "./preview.module.css";
 
 const Preview = ({ cards }) => {
   return (
     <section className={styles.preview}>
       <h1 className={styles.title}>Card Preview</h1>
-      {cards.map((item) => (
-        <section className={styles.card}>
-          <img
-            className={styles.profile}
-            src="./images/default_logo.png"
-            alt="profile"
-          />
-          <div className={styles.content}>
-            <h1 className={styles.text}>{item.name}</h1>
-            <p className={styles.text}>{item.corporation}</p>
-            <div className={styles.divider}></div>
-            <p className={styles.text}>{item.job}</p>
-            <p className={styles.text}>{item.email}</p>
-            <p className={styles.text}>{item.introduction}</p>
-          </div>
-        </section>
-      ))}
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card card={card} />
+        ))}
+      </ul>
     </section>
   );
 };
